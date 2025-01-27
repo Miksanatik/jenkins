ffdbc891b47a47cb7cf21a687bf42420448b3bbe
Miksanatik <sapanovich.aleksey@gmail.com>
2025-01-27 15:22:11 +0300

    first commit

---
+++ src/main/java/organisation/TestClass.java
@@ -0,0 +1,14 @@
+package organisation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestClass {
+    @Test
+    @DisplayName("Simple test")
+    public void test(){
+        assertTrue(true);
+    }
+}
