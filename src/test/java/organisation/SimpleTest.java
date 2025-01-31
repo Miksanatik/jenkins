a01f055ffa91f31bd1fadf2a7ce4569f60fcdc46
Miksanatik <sapanovich.aleksey@gmail.com>
2025-01-31 13:08:47 +0300

    update

--- src/test/java/organisation/SimpleTest.java
+++ src/test/java/organisation/SimpleTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TestClass {
+public class SimpleTest {
     @Test
     @DisplayName("Simple test")
     public void test(){
